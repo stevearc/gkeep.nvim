b09adce91006b581df13b02dcb209115fd16174b
Steven Arcangeli <stevearc@stevearc.com>
2021-11-12 17:57:16 -0800

    Lazy load gpsoauth the improve startup time

--- rplugin/python3/gkeep/__init__.py
+++ rplugin/python3/gkeep/__init__.py
@@ -1,5 +1,29 @@
+import importlib.util
 import subprocess
 import sys
+from typing import Any
+
+
+class LazyModule:
+    def __init__(self, module_name: str):
+        self._module_name = module_name
+        self._mod = None
+
+    @property
+    def mod(self) -> Any:
+        if self._mod is None:
+            del sys.modules[self._module_name]
+            self._mod = __import__(self._module_name)
+        return self._mod
+
+    def __getattr__(self, key: str) -> Any:
+        if key == "__spec__":
+            return importlib.util.find_spec(self._module_name)
+        return getattr(self.mod, key)
+
+
+# gpsoauth is crazy slow to import. This speeds up load time by quite a bit
+sys.modules["gpsoauth"] = LazyModule("gpsoauth")  # type: ignore[assignment]
 
 try:
     import gkeepapi
